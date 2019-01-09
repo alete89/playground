af93e54006f7a795e94f5e6a868ec7f6a334495d
alete89 <maildealete@gmail.com>
2019-01-08 21:45:54 -0300

    primer prueba con ECC

---
+++ mycript.py
@@ -0,0 +1,12 @@
+from Cryptodome.PublicKey import ECC
+
+a = ECC.generate(curve="P-256")
+print(a)
+
+f = open("myprivkey.pem", "wt")
+f.write(a.export_key(format="PEM"))
+f.close()
+
+o = open("myprivkey.pem", "rt")
+key = ECC.import_key(o.read())
+print(key)

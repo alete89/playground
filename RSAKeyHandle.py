e8b5433b47771569908a15b82f33c2b9c8c94c5a
alete89 <maildealete@gmail.com>
2019-01-10 13:17:52 -0300

    primeras pruebas con RSA

---
+++ RSAKeyHandle.py
@@ -0,0 +1,18 @@
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+unTexto = "Un mensaje de ejemplo.".encode("utf-8")
+unaKey = RSA.generate(1024)
+
+
+def encryptRsaKey(RsaKey, password):
+    return RsaKey.export_key(passphrase=password, pkcs=8, protection="scryptAndAES128-CBC")
+
+
+def exportKeyToFile(encryptedKey, path):
+    with open(path, "wb") as file:
+        file.write(encryptedKey)
+
+
+exportKeyToFile(encryptRsaKey(unaKey, "asd"), "encryptedKey.bin")

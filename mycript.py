d13bb2230e50a987287c4878bd6b618a98b32d82
alete89 <maildealete@gmail.com>
2019-01-10 10:26:59 -0300

    pep8

--- mycript.py
+++ mycript.py
@@ -1,7 +1,9 @@
 from Crypto.PublicKey import ECC
 
+
 def generateNewKey():
     return ECC.generate(curve="secp256r1")
+
 
 def importKeyFromFile(path, password=None):
     with open(path, "rt") as file:
@@ -10,15 +12,17 @@ def importKeyFromFile(path, password=None):
         except ValueError as err:
             print(err)
             print("quizás la clave está encriptada?")
-        
 
-def exportKeyToFile(key:ECC.EccKey, path):
+
+def exportKeyToFile(key: ECC.EccKey, path):
     with open(path, "wt") as file:
         file.write(key.export_key(format="PEM"))
 
-def exportKeyToFileEncrypted(key:ECC.EccKey, path, password):
+
+def exportKeyToFileEncrypted(key: ECC.EccKey, path, password):
     with open(path, "wt") as file:
-        file.write(key.export_key(format="PEM", passphrase=password, protection="PBKDF2WithHMAC-SHA1AndAES128-CBC"))
+        file.write(key.export_key(format="PEM", passphrase=password,
+                                  protection="PBKDF2WithHMAC-SHA1AndAES128-CBC"))
 
 
 alice_priv = generateNewKey()
@@ -38,5 +42,5 @@ alice_priv = generateNewKey()
 # exportKeyToFile(alice_public, "alice_public.pem")
 # exportKeyToFile(bob_public, "bob_public.pem")
 
-exportKeyToFileEncrypted(alice_priv, "alice_encrypted_private.pem","asd123")
+exportKeyToFileEncrypted(alice_priv, "alice_encrypted_private.pem", "asd123")
 alice_importada = importKeyFromFile("alice_encrypted_private.pem", "asd123")

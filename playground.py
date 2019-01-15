507b0893e4f0b7e33a4864e30a1a6238734be5ea
alete89 <maildealete@gmail.com>
2019-01-15 14:48:06 -0300

    ejemplo incompleto enviar mensaje encriptado

--- playground.py
+++ playground.py
@@ -1,18 +1,27 @@
 import repoUsuarios as repo
 from Usuario import Usuario
 import RSAKeyHandle as rsa
+import AesPlayground as aes
 
 keysize = 1024
 
-usuarios =[
-        Usuario("ale", rsa.getKeyPair(keysize).publickey()),
-        Usuario("pedro", rsa.getKeyPair(keysize).publickey()),
-        Usuario("carlos", rsa.getKeyPair(keysize).publickey()),
-        Usuario("alice", rsa.getKeyPair(keysize).publickey()),
-        Usuario("bob", rsa.getKeyPair(keysize).publickey())
-    ]
+usuarios = [
+    Usuario("ale", rsa.getKeyPair(keysize)),
+    Usuario("pedro", rsa.getKeyPair(keysize)),
+    Usuario("carlos", rsa.getKeyPair(keysize)),
+    Usuario("alice", rsa.getKeyPair(keysize)),
+    Usuario("bob", rsa.getKeyPair(keysize))
+]
 repo.agregarUsuarios(usuarios)
 
-rsa.exportKeyToFile(rsa.encryptRsaKey(usuarios[0].pubkey),"ale.pem")
-print(repo.getUsernameList())
-print(repo.getPubKeyFromUsername("ale"))
+# rsa.exportKeyToFile(rsa.encryptRsaKey(usuarios[0].pubkey),"ale.pem")
+# print(repo.getUsernameList())
+# print(repo.getPubKeyFromUsername("ale"))
+
+# Ale le envía un mensaje a Pedro
+mensaje = "Un mensaje de prueba."
+destination_pubkey = repo.getPubKeyFromUsername("pedro")
+session_key = rsa.get_random_bytes(16)
+cipher_rsa = rsa.PKCS1_OAEP.new(destination_pubkey)
+enc_session_key = cipher_rsa.encrypt(session_key)  # lista para mandar a Pedro
+aes.encryptBytesWithAes(mensaje, session_key, "data_para_pedro.bin")
